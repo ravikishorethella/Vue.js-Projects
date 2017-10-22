d469f0900ea35dba8438a9d724ee63bfd2d68dec
ravikishorethella <ravikishorethella@gmail.com>
2017-10-21 21:48:09 -0400

    folder name changed

---
+++ app.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#app',
+  data: {
+    ingredients: ['meat', 'fruit', 'vegetables'],
+    persons: [
+      { name: 'max', age: 27, color: 'red' },
+      { name: 'Anna', age: 'unknnow', color: 'blue' },
+    ],
+  },
+});
